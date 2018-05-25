7e55caf15885a27c59367e9844f73a32f3dda799
monalip <monalipande9@gmail.com>
2018-05-25 14:02:28 +0200

    Added class folder and updated the run script accordingly

--- DiningPhil/runAspectJDiningPhil.sh
+++ DiningPhil/runAspectJDiningPhil.sh
@@ -1,1 +1,1 @@
-java -cp ../aspectj1.8/lib/aspectjrt.jar:target/classes/ se.kth.maven.quickstart.DiningPhil
+java -cp ../aspectj1.8/lib/aspectjrt.jar:classes/ se.kth.maven.quickstart.DiningPhil
--- DiningPhil/runAspectJweavingDiningPhil.sh
+++ DiningPhil/runAspectJweavingDiningPhil.sh
@@ -1,2 +1,2 @@
-java -cp -Xbootclasspath/p:../aspectj1.8/lib/aspectjrt.jar:$JAVA_HOME/jre/lib/rt.jar:target/classes/ se.kth.maven.quickstart.DiningPhil
+java -cp -Xbootclasspath/p:../aspectj1.8/lib/aspectjrt.jar:$JAVA_HOME/jre/lib/rt.jar:classes/ se.kth.maven.quickstart.DiningPhil
 
