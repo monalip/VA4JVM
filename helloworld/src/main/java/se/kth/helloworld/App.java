e0b91b0d506edbf13dbdc5a1230532f460aa60b5
monalip <monalipande9@gmail.com>
2018-06-25 15:22:23 +0200

    Created new helowworld program and added jpf configuration file to generate the jpf trace

---
+++ helloworld/src/main/java/se/kth/helloworld/App.java
@@ -0,0 +1,13 @@
+package se.kth.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
