90db56925f5c51b3a483ecbb60043078040d27e6
monalip <monalipande9@gmail.com>
2018-07-13 15:36:49 +0200

    Modified POM.xml to add the dependecy of the jgraphx so now it build and execute withouting installing script

--- HelloWorld_VA4JVM/build.sh
+++ HelloWorld_VA4JVM/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mvn install:install-file -Dfile=jpf-visual/lib/jgraphx.jar -DgroupId=jgraphx -DartifactId=graphixfile -Dversion=1.0 -DgeneratePom=true -DpomFile=jpf-visual/pom.xml
-mvn install:install-file -Dfile=helloworld/lib/jgraphx.jar -DgroupId=jgraphx -DartifactId=graphixfile -Dversion=1.0 -DgeneratePom=true -DpomFile=helloworld/pom.xml
-mvn clean install
+mvn clean 
+mvn install
