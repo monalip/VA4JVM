12d15f645fb005d73498bfb774fcb445ac6317a1
monalip <monalipande9@gmail.com>
2018-09-03 11:58:45 +0200

    changed the program

--- HelloWorld_VA4JVM/jpf-visual/helloworld/src/App.java
+++ HelloWorld_VA4JVM/jpf-visual/helloworld/src/App.java
@@ -1,6 +1,6 @@
 
 
-public class App  
+public class App 
 {
 	static int a = 3;
 	static int b = 5;
@@ -14,17 +14,22 @@ public class App
 	
 	public static void main( String[] args )
     {
+       String name = "Monali Pande";
 		App app = new App();
-		int result;
-        result = add(a, b);
-        
-        MyThread t = new MyThread();
- 
-       t.start();
-      
+		System.out.println("App values: "+app.a);
+		int r = add(2,3);
+		MyThread t = new MyThread(name);
+		
+		new Thread(t).start();
+		try {
+			new Thread(t).join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print("Finish");
+		r=1/0;
        
-       
-
     }
 
 	private static int add(int a2, int b2) {
@@ -34,26 +39,40 @@ public class App
 	
 
 }
-class MyThread extends Thread
-{	public String name;
+class MyThread implements Runnable
+{	
+	public String name;
 	
-	public MyThread() {
-		this.name = "Monali";
+	public MyThread(String name) {
+		this.name = name;
 	}
 	public void run()
 	{
 		int m,n,r;
-		m = 1;
+		m = 4;
 		n = 2;
 		r = threadadd(m,n);
+		r = threadsub(m,n);
 		System.out.println("ChildThread "+r);
+		synchronized (name) 
+		{
+            try{
+                System.out.println(name+" waiting to get notified at time:"+System.currentTimeMillis());
+                name.wait(2000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+		}
 		
 		
 	}
-	 private int threadadd(int m, int n) {
-		 int result;
-		 result = 4/0;
+	 private synchronized int threadadd(int m, int n) {
 			return m + n;
+			
+			
+		}
+	 private int threadsub(int m, int n) {
+			return m - n;
 			
 			
 		}
